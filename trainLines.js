073fe46626e16af0e1fe7e84254ad46ef7eb9b33
Elliott Nottingham <elliottnottingham07@gmail.com>
2024-08-29 20:06:32 -0500

    created a game where a person can travel to different trainlines based on the color of the trainline

--- trainLines.js
+++ trainLines.js
@@ -10,3 +10,62 @@ function ask(questionText) {
         readlineInterface.question(questionText, resolve);
     });
 }
+
+//YOUR CODE GOES HERE
+
+class Line {
+    constructor(color) {
+        this.color = color;
+        this.connections = [];
+    }
+    
+    addConnections(lines) {
+        this.connections.push(...lines);
+    }
+}
+
+class Person {
+    constructor(startingLine) {
+        this.name = null;
+        this.currentLine = startingLine
+    }
+
+    transferLines(line) {
+        this.currentLine = line;
+    }
+}
+
+let blue = new Line("blue");
+let green = new Line("green");
+let yellow = new Line("yellow");
+let red = new Line("red");
+let purple = new Line("purple");
+
+blue.addConnections([green, red]);
+green.addConnections([blue, yellow]);
+yellow.addConnections([purple, green, red]);
+red.addConnections([blue, yellow]);
+purple.addConnections([yellow]);
+
+let user = new Person(blue);
+
+async function main() {
+    console.log("Hello! Welcome to our subway!")
+    user.name = await ask("What is your name? ");
+    console.log(`Hello ${user.name}! You are on the ${user.currentLine.color} line.`)
+
+    let destLine = undefined;
+    do {
+    const destination = await ask("Where would you like to go? ");
+    destLine = user.currentLine.connections.find((line)=> line.color === destination);
+        if (destLine === undefined) {
+        console.log(`You can't go from ${user.currentLine.color} to ${destination}.`);
+    }
+    }while (destLine === undefined)
+
+    user.transferLines(destLine);
+    console.log(`You are now on the ${user.currentLine.color} line.`)
+        process.exit();
+    }
+
+main();
